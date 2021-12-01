1b8410d8dccc90767989d7f85e23a7ef17e67873
Roshni1809 <roshni18shukla@gmail.com>
2021-12-02 01:39:02 +0530

    updates

--- src/Components/Home Pages/HomeContent.jsx
+++ src/Components/Home Pages/HomeContent.jsx
@@ -23,7 +23,7 @@ const HomeContent = () => {
             <br></br><br></br>
             Madhubani artists make their paints and brushes from locally available materials. The handmade paper is treated with cow dung, which helps maintain the strong color of the natural pigments. Charcoal is mixed with water to outline the images, and crushed flowers and plants are mixed with powdered rice to make colorful pastes. No space remains empty on a Madhubani work—gaps are delicately filled in with flowers, animals, birds and geometric designs.
             <br></br><br></br>
-            Available at Laasya Art is a carefully selected collection of original Madhubani (Mithila) paintings by internationally recognized artists, including National Award recipients. Some are mentees of Smt. Jagadamba Devi, who won the Padma Shri in 2011 and was the first Mithila-born artist to receive the National Award for Madhubani painting in 1982. These artists have exhibited their paintings throughout Europe and the US.
+            Available at  is a carefully selected collection of original Madhubani (Mithila) paintings by internationally recognized artists, including National Award recipients. Some are mentees of Smt. Jagadamba Devi, who won the Padma Shri in 2011 and was the first Mithila-born artist to receive the National Award for Madhubani painting in 1982. These artists have exhibited their paintings throughout Europe and the US.
             <br></br><br></br>
             You are welcome to buy Madhubani (Mithila) paintings online or view them at our gallery in Palo Alto (San Francisco Bay Area). All artworks include a certificate of authenticity.
             <br></br>
