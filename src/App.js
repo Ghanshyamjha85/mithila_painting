5d96a6562cf85a4596981ac3b0f1ad4041906856
Roshni1809 <roshni18shukla@gmail.com>
2021-12-02 00:24:36 +0530

    updates in about us

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Details from './Components/Details Pages/Details';
 import Header from './Components/Header';
 import Galary from './Components/Galary Pages/Galary';
 import Home from './Components/Home Pages/Home.jsx';
-// import Collections from './Components/Collections';
+import Collections from './Components/Collections';
 import AboutUs from './Components/AboutUs';
 
 function App() {
@@ -15,8 +15,8 @@ function App() {
             <Routes>
                 <Route path='/home' element={<Home />} />
                 <Route path="/" element={<Navigate replace to="/home" />} />
-              
-                <Route path='/collections' element = {<Galary />} />
+                <Route path='/galary' exact element={<Galary />} />
+                <Route path='/collections' element = { <Collections />} />
                 <Route path='/about' element = { <AboutUs />} />
                 <Route path='/:id' exact element={< Details />} />
             </Routes>
